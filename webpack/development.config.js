cd7148056d5ea600706c4882ff1476d87066480a
Dmitry Rodnikov <dim.reg@mail.ru>
2017-08-23 22:49:17 +0300

    Добавил CSS лодеры

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const {
-  HotModuleReplacementPlugin
-} = require('webpack');
+const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = merge([{
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    path.join(__dirname, '../src/index.js')
+    path.join(__dirname, '../src/index.js'),
   ],
   output: {
     path: path.join(__dirname, '../www'),
     filename: 'bundle.js',
-    publicPath: 'http://localhost:8080/'
+    publicPath: 'http://localhost:8080/',
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
   },
   plugins: [
     new HotModuleReplacementPlugin(),
@@ -31,6 +39,6 @@ module.exports = merge([{
     stats: 'errors-only',
     host: 'localhost',
     port: 8080,
-    contentBase: path.join(__dirname, '../www')
-  }
+    contentBase: path.join(__dirname, '../www'),
+  },
 }]);
