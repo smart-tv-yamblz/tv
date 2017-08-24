7efff3a959f83648e1433afe9519ea197d0e71a7
Dmitry Rodnikov <dim.reg@mail.ru>
2017-08-24 13:52:01 +0300

    Убрал пустые строки перед импортом стилей

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ContentA from '../contentA';
 import ContentB from '../contentB';
-
 import './style.scss';
 
 const Content = () => (
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Menu from '../menu';
 import Content from '../content';
-
 import './style.scss';
 
 const Layout = () => (
